aef406e4d7f956e845941c5eb8f7bd765d977e6e
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-09-08 14:45:27 +0200

    working

---
+++ ProjectPigna_Console/EsMelangeur.h
@@ -0,0 +1,104 @@
+#ifndef _ESMELANGEUR_
+#define _ESMELANGEUR_
+// à modifier suivant architecture locale
+#include "..\..\..\Codes\CPP\Include\NIDAQmx.h"
+#include <string>
+
+using namespace std;
+
+class CEsMelangeur
+{
+private:
+		// masse de produit
+	int m_poids;
+		// image des capteurs
+	bool m_capteur_bas_R1;			// réservoir 1
+	bool m_capteur_bas_R2;			// réservoir 2
+	bool m_capteur_bas_R3;			// réservoir 3
+	bool m_capteur_niveau_bas;		// cuve de refroidissement
+	bool m_capteur_niveau_haut;		// cuve de refroidissement
+	bool m_marche;					// panneau de commande
+	bool m_arret;					// panneau de commande
+	bool m_manuel_auto;				// panneau de commande
+		// image des actionneurs
+	bool m_vanne_pvc_base;			// vanne réservoir R1
+	bool m_vanne_pvc_base_FD;		// vanne réservoir R1 Faible Débit
+	bool m_vanne_plastifiant;		// vanne réservoir R2
+	bool m_vanne_lubrifiant;		//vanne réservoir R3
+	bool m_vanne_vidange;			// vidange malaxeur
+	bool m_malaxeur;				// agitateur malaxeur
+	bool m_evacuation;				// pompe vidange cuve
+	bool m_voyant_rouge;			//panneau de commande
+		// identificateurs taches DAQmx
+			// acquisition des données analogiques
+	TaskHandle m_tache_lecture_ana;
+			// acquisition des données numériques
+	TaskHandle m_tache_lecture_tor;
+			// restitution des données numériques
+	TaskHandle m_tache_ecriture_tor;
+public:
+		/* Constructeur
+		**	initialise les attributs à 0
+		*/
+	CEsMelangeur();
+	
+		/*
+		** Initialisation de l'interface d'E/S NiDAQ
+		*/
+	int32 initEsMelangeur();
+	
+		/*
+		** Clotûre de l'interface d'E/S NiDAQ
+		*/
+	int32 fermerEsMelangeur();
+	
+		/*
+		** Acquisition des données analogiques et numériques
+		** L'automaticien demande à ce que TOUTES les entrées
+		** soient acquises au même moment.
+		** Donc sur un port 8 bits on lit un octet.
+		*/
+	int32 lireEntrees();
+	
+		/*
+		** Restitution des données numériques
+		** L'automaticien demande à ce que TOUTES les sorties
+		** soient mises à jour au même moment.
+		** Donc sur un port 8 bits on écrit un octet.
+		*/
+	int32 majSorties();
+	
+		/* Accesseurs
+		** Permettent d'obtenir les valeurs des attributs privés de la classe
+		** Dans ce modèle seule l'image des données en entrée a un sens.
+		*/
+	int getPoids();
+	bool getCapteurBasR1();
+	bool getCapteurBasR2();
+	bool getCapteurBasR3();
+	bool getCapteurNiveauBas();
+	bool getCapteurNiveauHaut();
+	bool getMarche();
+	bool getArret();
+	bool getManuelAuto();
+	
+		/* Mutateurs
+		** Permettent d'affecter une valeur aux attributs privés de la classe
+		** Dans ce modèle seule l'image des données en sortie a un sens.
+		*/
+	void setVannePVCBase(bool etat);
+	void setVannePVCBaseFD(bool etat);
+	void setVannePlastifiant(bool etat);
+	void setVanneLubrifiant(bool etat);
+	void setVanneVidange(bool etat);
+	void setMalaxeur(bool etat);
+	void setEvacuation(bool etat);
+	void setVoyantRouge(bool etat);
+	
+		/* Récupération du texte correspondant au numéro d'erreur retouné par les fonctions DAQmx
+		** On passe le numéro d'erreur
+		** On récupère la chaîne en string
+		*/
+	string texteErreur(int32 code);
+};
+#endif
--- ProjectPigna_Console/main.cpp
+++ ProjectPigna_Console/main.cpp
@@ -3,7 +3,10 @@
 
 #include "NIDAQmx.h"
 
-int main()
+/*
+Turn on a led
+*/
+int Q2_2()
 {
 	TaskHandle valve2Handle;
 	int32 error;
@@ -17,20 +20,20 @@ int main()
 
 	// 2 : Create Digital out
 	std::cout << "...: Create DigitalOut :..." << std::endl;
-	if ((error=DAQmxCreateDOChan(valve2Handle, "Dev1/port0", "toto", DAQmx_Val_ChanForAllLines)) < 0)
+	if ((error = DAQmxCreateDOChan(valve2Handle, "Dev1/port0", "", DAQmx_Val_ChanForAllLines)) < 0)
 	{
 		std::cout << "ERROR Create DigitalOut" << std::endl;
 		std::cout << (int)error << std::endl;
 		return -1;
 	}
-	
+
 	// 3 : Start Task
 	if (DAQmxStartTask(valve2Handle) < 0)
 	{
 		std::cout << "ERROR Start Task" << std::endl;
 		return -1;
 	}
-		
+
 	// 4 : Write
 	if (DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, 1, NULL) < 0)
 	{
@@ -40,7 +43,7 @@ int main()
 
 	Sleep(2000);
 
-	if(DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, 0, NULL) < 0)
+	if (DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, 0, NULL) < 0)
 	{
 		std::cout << "ERROR Write Digital (2)" << std::endl;
 		return -1;
@@ -61,5 +64,86 @@ int main()
 	}
 
 	std::cout << "...: Program Finished Ok :...";
+
+}
+
+/*
+Turn on 2 leds, and turn off with 2sec interval
+*/
+int Q2_3()
+{
+	TaskHandle valve2Handle;
+	int32 error;
+	// 1 : Create Task
+	std::cout << "...: Create Task :..." << std::endl;
+	if (DAQmxCreateTask("", &valve2Handle) < 0)
+	{
+		std::cout << "ERROR Create Task" << std::endl;
+		return -1;
+	}
+
+	// 2 : Create Digital out
+	std::cout << "...: Create DigitalOut :..." << std::endl;
+	if ((error = DAQmxCreateDOChan(valve2Handle, "Dev1/port0", "", DAQmx_Val_ChanForAllLines)) < 0)
+	{
+		std::cout << "ERROR Create DigitalOut" << std::endl;
+		std::cout << (int)error << std::endl;
+		return -1;
+	}
+
+	// 3 : Start Task
+	if (DAQmxStartTask(valve2Handle) < 0)
+	{
+		std::cout << "ERROR Start Task" << std::endl;
+		return -1;
+	}
+
+	// 4 : Write
+	uint32_t valveAction = 0x03;  // Both bits 0 and 1 set (binary 0011)
+	if (DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, valveAction, NULL) < 0)
+	{
+		std::cout << "ERROR Write Digital (1)" << std::endl;
+		return -1;
+	}
+
+	Sleep(2000);
+
+	valveAction = 0x02;
+	if (DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, valveAction, NULL) < 0)
+	{
+		std::cout << "ERROR Write Digital (2)" << std::endl;
+		return -1;
+	}
+
+	Sleep(2000);
+
+	valveAction = 0x00;
+	if (DAQmxWriteDigitalScalarU32(valve2Handle, true, 10, valveAction, NULL) < 0)
+	{
+		std::cout << "ERROR Write Digital (2)" << std::endl;
+		return -1;
+	}
+
+	// 5 : Stop Task
+	if (DAQmxStopTask(valve2Handle) < 0)
+	{
+		std::cout << "ERROR Stop Task" << std::endl;
+		return -1;
+	}
+
+	// 6 : Clear Task
+	if (DAQmxClearTask(valve2Handle) < 0)
+	{
+		std::cout << "ERROR Clear Task" << std::endl;
+		return -1;
+	}
+
+	std::cout << "...: Program Finished Ok :...";
+
 	return 0;
+}
+
+int main()
+{
+	
 }
