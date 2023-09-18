53f4a27581dc9fe7a8c82ea7cfda87063724e28b
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-09-18 16:24:35 +0200

    catch2

--- ClasseSec/main.cpp
+++ ClasseSec/main.cpp
@@ -1,90 +1,109 @@
 #include "Personne.h"
 #include <iostream>
 #include <Windows.h>
-//#include "catch2/catch_test_macros.hpp"
-
-//#define CATCH_CONFIG_MAIN  // Ceci indique à Catch de fournir une fonction main()
 //#include "catch_amalgamated.hpp"
-//
-//int add(int a, int b);
-//int subtract(int a, int b);
-//
-//TEST_CASE("Test des fonctions add et subtract", "[math]") {
-//    SECTION("Test de la fonction add") {
-//        REQUIRE(add(2, 3) == 5);
-//        REQUIRE(add(-2, 2) == 0);
-//        REQUIRE(add(-2, -3) == -5);
-//    }
-//
-//    SECTION("Test de la fonction subtract") {
-//        REQUIRE(subtract(5, 3) == 2);
-//        REQUIRE(subtract(2, 2) == 0);
-//        REQUIRE(subtract(2, 5) == -3);
-//    }
-//};
+
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"
+
+int add(int a, int b);
+int subtract(int a, int b);
+
 int add(int x, int y)
 {
     return x + y;
 }
 
-//TEST_CASE("Factorials are computed", "[factorial]") {
-//    int z = 1;
-//    int r = 5;
-//
-//    REQUIRE(add(r, z) < 15);
-//}
+int subtract(int x, int y)
+{
+    return x - y;
+}
 
+TEST_CASE("add are computed", "[add]")
+{
+    REQUIRE(add(1, 0) == 5);
+    REQUIRE(add(2, 0) == 2);
+    REQUIRE(add(1, 2) == 3);
+    REQUIRE(add(10, 5) == 15);
+}
 
-int main() {
-    std::string s;
+TEST_CASE("subtract are computed", "[subtract]")
+{
+    REQUIRE(subtract(1, 0) == 1);
+    REQUIRE(subtract(2, 1) == 1);
+    REQUIRE(subtract(3, 2) == 1);
+    REQUIRE(subtract(10, 5) == 5);
+}
 
-    std::cout << "Entrez votre num\202ro de s\202cu (15 chiffres) [d: pour exemple] \n";
-    std::cin >> s;
-    if (s == "d")
-        s = "103061730041813";
-
-    Personne personne(s);
-
-    if (s.length() != 15 || !personne.verifCleNumSecu())
-    {
-        std::cout << "Ce n'est pas un bon num\202ro !!! Imposteur !!!!!!!!!!!!!!";
-        return 0;
+TEST_CASE("Test de la classe Personne", "[Personne]") {
+    Personne personne("103061730041813");
+    
+    SECTION("Vérification du numéro de sécurité sociale") {
+        REQUIRE(personne.verifCleNumSecu() == true);
     }
- 
 
-    std::cout << "Entrez votre adresse : ";
-    std::cin >> s;
-    personne.setAdresse(s);
+    SECTION("Test de l'adresse") {
+        personne.setAdresse("123 rue de la Paix");
+        REQUIRE(personne.getAdresse() != "");
+    }
 
-    int x;
-    std::cout << "Entrez votre nombre d'enfants : ";
-    std::cin >> x;
-    personne.setNbe(x);
-
-    std::cout << std::endl << "--------------------------------------------------------------------------------------" << std::endl;
-    std::cout << std::endl << "\t ....:ANALYSE DU FUTUR EN COURS:...." << std::endl;
-    Sleep(1500);
-    std::cout << "Traduction binaire hexa du futur... ";
-    Sleep(500);
-    std::cout << "... OK ";
-    Sleep(250);
-    std::cout << std::endl << "Creation d'une classe abstraite contenant une m\202thode virtuelle... ";
-    Sleep(500);
-    std::cout << "... OK ";
-    Sleep(250);
-    std::cout << std::endl << "Interrogation de la base de donn\202es de la NASA... ";
-    Sleep(500);
-    std::cout << "... OK ";
-    Sleep(250);
-    std::cout << std::endl << "R\202cup\202ration des donn\202es via un protocole de TCP fiable... ";
-    Sleep(500);
-    std::cout << "... OK ";
-    Sleep(1500);
-    std::cout << std::endl << std::endl << "\t ....:ANALYSE DU FUTUR TERMINEE:...." << std::endl << std::endl;
-
-    std::cout << std::endl << personne.toString() << std::endl;
+    // Ajoutez d'autres sections pour tester d'autres fonctionnalités de votre classe.
+}
+int main(int argc, char* argv[]) {
 
 
+
+    //std::string s;
+
+    //std::cout << "Entrez votre num\202ro de s\202cu (15 chiffres) [d: pour exemple] \n";
+    //std::cin >> s;
+    //if (s == "d")
+    //    s = "103061730041813";
+
+    //Personne personne(s);
+
+    //if (s.length() != 15 || !personne.verifCleNumSecu())
+    //{
+    //    std::cout << "Ce n'est pas un bon num\202ro !!! Imposteur !!!!!!!!!!!!!!";
+    //    return 0;
+    //}
+
+
+
+
+    //std::cout << "Entrez votre adresse : ";
+    //std::cin >> s;
+    //personne.setAdresse(s);
+
+    //int x;
+    //std::cout << "Entrez votre nombre d'enfants : ";
+    //std::cin >> x;
+    //personne.setNbe(x);
+
+    //std::cout << std::endl << "--------------------------------------------------------------------------------------" << std::endl;
+    //std::cout << std::endl << "\t ....:ANALYSE DU FUTUR EN COURS:...." << std::endl;
+    //Sleep(1500);
+    //std::cout << "Traduction binaire hexa du futur... ";
+    //Sleep(500);
+    //std::cout << "... OK ";
+    //Sleep(250);
+    //std::cout << std::endl << "Creation d'une classe abstraite contenant une m\202thode virtuelle... ";
+    //Sleep(500);
+    //std::cout << "... OK ";
+    //Sleep(250);
+    //std::cout << std::endl << "Interrogation de la base de donn\202es de la NASA... ";
+    //Sleep(500);
+    //std::cout << "... OK ";
+    //Sleep(250);
+    //std::cout << std::endl << "R\202cup\202ration des donn\202es via un protocole de TCP fiable... ";
+    //Sleep(500);
+    //std::cout << "... OK ";
+    //Sleep(1500);
+    //std::cout << std::endl << std::endl << "\t ....:ANALYSE DU FUTUR TERMINEE:...." << std::endl << std::endl;
+
+    //std::cout << std::endl << personne.toString() << std::endl;
+
+    Catch::Session().run(argc, argv);
 
     return 0;
 }
