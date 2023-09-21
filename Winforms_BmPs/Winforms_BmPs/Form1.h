e64fa671f2a23488660b09fbb5f6e5b751ad188e
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-09-21 20:13:45 +0200

    Update Form1.h

--- Winforms_BmPs/Winforms_BmPs/Form1.h
+++ Winforms_BmPs/Winforms_BmPs/Form1.h
@@ -37,7 +37,8 @@ namespace CppCLRWinFormsProject {
 
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::Panel^ panel2;
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ btn_importer;
+
 	protected:
 
 	private:
@@ -55,7 +56,7 @@ namespace CppCLRWinFormsProject {
 		{
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->btn_importer = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->panel2->SuspendLayout();
 			this->SuspendLayout();
@@ -74,21 +75,24 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->panel2->BackColor = System::Drawing::Color::DimGray;
 			this->panel2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->panel2->Controls->Add(this->button1);
+			this->panel2->Controls->Add(this->btn_importer);
 			this->panel2->Cursor = System::Windows::Forms::Cursors::Default;
 			this->panel2->Location = System::Drawing::Point(-7, 0);
 			this->panel2->Name = L"panel2";
 			this->panel2->Size = System::Drawing::Size(1717, 97);
 			this->panel2->TabIndex = 0;
 			// 
-			// button1
+			// btn_importer
 			// 
-			this->button1->Location = System::Drawing::Point(18, 11);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(291, 73);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
+			this->btn_importer->BackColor = System::Drawing::Color::Transparent;
+			this->btn_importer->Font = (gcnew System::Drawing::Font(L"Times New Roman", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn_importer->Location = System::Drawing::Point(38, 20);
+			this->btn_importer->Name = L"btn_importer";
+			this->btn_importer->Size = System::Drawing::Size(270, 45);
+			this->btn_importer->TabIndex = 2;
+			this->btn_importer->Text = L"Importer";
+			this->btn_importer->UseVisualStyleBackColor = false;
 			// 
 			// Form1
 			// 
@@ -100,7 +104,6 @@ namespace CppCLRWinFormsProject {
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->pictureBox1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
-			this->ImeMode = System::Windows::Forms::ImeMode::NoControl;
 			this->MaximizeBox = false;
 			this->MinimizeBox = false;
 			this->Name = L"Form1";
