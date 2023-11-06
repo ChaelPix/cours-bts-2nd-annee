091762ce43e97fccc425e667900e3858eb09befb
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-11-06 17:48:46 +0100

    random

--- TP_cpp/sockets_tuto/main.cpp
+++ TP_cpp/sockets_tuto/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <cstring>
 using namespace std;
 
 typedef unsigned int uint;
@@ -12,8 +13,48 @@ typedef unsigned char uchar;
 
 const ushort PORT_NUM = 55555;
 
+#include <random>
+struct mesure {
+    char date[26];
+    float valTemp;
+    float valHum;
+};
+
+
+void prendreMesure(mesure& m) {
+    double valTemp = 0.0;
+    double valHum = 0.0;
+
+    // Tirage aléatoire de nombres
+    std::random_device rd;
+    std::mt19937 mt(rd()); // Mersenne Twister
+
+    // tirage pour la valeur du capteur de température entre 15°C et 25°C, tirage d'un double
+    std::uniform_real_distribution<double> dist(15.0, 25.0);
+    valTemp = dist(mt);
+
+    // tirage pour la valeur du capteur d'hygrométrie entre 10%RH et 90%RH, tirage d'un entier
+    valHum = rand() % 80 + 10;
+
+    // récupération de la date et l'heure courante
+    time_t tempsCourant = time(nullptr); 
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &tempsCourant);
+    char buffer[26];
+    asctime_s(buffer, sizeof(buffer), &timeinfo);
+    std::string dateHeureMesure(buffer);
+
+    // remplissage de la structure avec les valeurs précédentes
+    strcpy_s(m.date, sizeof(m.date), buffer);
+    m.valHum = valHum;
+    m.valTemp = valTemp; 
+}
+
 int main(void)
 {
+    // Initialisation du générateur de nombres aléatoires
+    srand(static_cast<unsigned int>(time(NULL)));
+
     WORD nVersion = MAKEWORD(2, 2);
     WSADATA donneeWS;
 
@@ -66,23 +107,17 @@ int main(void)
     addr_len = sizeof(adr_client);
     ids_connect = accept(ids_ecoute, (struct sockaddr*)&adr_client, &addr_len);
 
+    mesure m;
+    prendreMesure(m);
 
-    time_t tempsCourant = time(nullptr); // obtenir l'heure actuelle
-    struct tm timeinfo; 
-    localtime_s(&timeinfo, &tempsCourant);
-    char buffer[26]; 
-    asctime_s(buffer, sizeof(buffer), &timeinfo);
-
-    std::string message(buffer); 
-
-    if ((nb_car_emis = send(ids_connect, message.c_str(),
-        message.size() + 1, 0)) == SOCKET_ERROR)
+   if ((nb_car_emis = send(ids_connect, reinterpret_cast<char*>(&m), sizeof(m), 0)) == SOCKET_ERROR)
     {
         cout << "Echec transmission " << endl;
         exit(1);
     }
+
     cout << "nb car emis par ce serveur : " << nb_car_emis << endl;
-    cout << "trame transmise par ce serveur: " << message << endl;
+    cout << "trame transmise par ce serveur: " << reinterpret_cast<char*>(&m) << endl;
 
     // fermeture des sockets ouverts
     closesocket(ids_ecoute);
